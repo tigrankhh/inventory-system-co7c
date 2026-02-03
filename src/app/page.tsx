decf23f8bda6c0da51bc16db8cf44a151bc1008c
tigrankhh <khachtik2606@gmail.com>
2026-02-03 06:29:09 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import { supabase } from '../../libsupabase';
+import { supabase } from '@/libsupabase';
 import { useRouter } from 'next/navigation';
 
 export default function LoginPage() {
