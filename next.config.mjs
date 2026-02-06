d8f86080eeb4bf35a5ccb0f49f567e2cbab1d7db
tigrankhh <khachtik2606@gmail.com>
2026-02-06 12:00:20 +0400

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* Мы убрали output: 'export', так как для Supabase Auth нужен серверный рантайм */
-  images: { 
-    unoptimized: true 
-  }
-};
-
-export default nextConfig;
